a36a9af4537aafdff23e7c2a5925f41593c58453
Tim Moses <trmoses42@gmail.com>
2021-11-16 22:54:06 -0600

    Solution

---
+++ firstNonRepeatedCharacter.js
@@ -0,0 +1,26 @@
+/*
+ * Given an arbitrary input string, return the first nonrepeated character in
+ * the string. For example:
+ *   firstNonRepeatedCharacter('ABA'); // => 'B'
+ *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
+ */
+
+ var firstNonRepeatedCharacter = function(string, prevChar) {
+  // TODO: your solution here
+  if (typeof string !== 'string') {
+    return null;
+  }
+  if (string.length === 0) {
+    return null;
+  }
+  var strArr = string.toUpperCase().split('');
+  var currentChar = strArr.shift();
+  var prevChar = prevChar || '';
+
+  if (strArr.indexOf(currentChar) === -1 && prevChar !== currentChar) {
+    return currentChar;
+  } else {
+    prevChar = currentChar;
+    return firstNonRepeatedCharacter(strArr.join(''), prevChar);
+  }
+};
